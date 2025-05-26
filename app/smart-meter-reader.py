91896c328da0dcc473440ed2e94393bb97a5330f
Ciaran Corson <ciaran.corson@gmail.com>
2025-05-26 17:11:07 +0100

    initial work to helmify meter readings automation

---
+++ app/smart-meter-reader.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import request
+
+from processor import MeterReader
+from metrics import Metrics
+
+from waitress import serve
+import prometheus_client
+from prometheus_client import make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+prometheus_client.REGISTRY.unregister(prometheus_client.GC_COLLECTOR)
+prometheus_client.REGISTRY.unregister(prometheus_client.PLATFORM_COLLECTOR)
+prometheus_client.REGISTRY.unregister(prometheus_client.PROCESS_COLLECTOR)
+metrics = Metrics()
+
+app = Flask(__name__)
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
+
+
+@app.route('/daily')
+def get_daily_consumption():
+    reader = MeterReader()
+    daily_consuption = reader.get_energy_consumption()
+    metrics.populate_metrics(daily_consuption)
+    return daily_consuption
+
+def register_prom_metrics():
+    metrics.collect()
+
+if __name__ == "__main__":
+    register_prom_metrics()
+    serve(app, host="0.0.0.0", port=8080)
